c2affdbfc0bfb8b1cbb281ca96c7c61027933d3b
sungyong-lim <dlatjddyd524@gmail.com>
2022-07-07 15:51:28 +0900

    DataType Quiz Update

---
+++ Kotlin/src/main/kotlin/quiz/1.quiz.kt
@@ -0,0 +1,24 @@
+package quiz
+
+// 변수와 데이터 유형 퀴즈
+
+
+fun main() {
+    var name: String = "Android Masterclass" // String 형식
+    val floatNum: Float= 13.37F // Float 형식
+    var doubleNum: Double = 3.14159265358979 // Double 형식
+    var byteNum: Byte = 25 //Byte 형식
+    var shortNum: Short = 2020 //Int 형식
+    var longNum: Long = 188_812_345_67 // Long 형식
+    var booleanTrue: Boolean = true //Boolean 형식
+    var charA: Char = 'a' // Char 형식
+
+    println(name)
+    println(floatNum)
+    println(doubleNum)
+    println(byteNum)
+    println(shortNum)
+    println(longNum)
+    println(booleanTrue)
+    println(charA)
+}
