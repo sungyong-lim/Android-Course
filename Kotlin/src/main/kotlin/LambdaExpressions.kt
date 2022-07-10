57bc542d60ff00d01727b74b7d8605a8030b574e
sungyong-lim <dlatjddyd524@gmail.com>
2022-07-10 15:14:12 +0900

    Lambda Expressions Update

---
+++ Kotlin/src/main/kotlin/LambdaExpressions.kt
@@ -0,0 +1,23 @@
+/** Lambda Expressions**/
+
+/*
+    - Syntax : { variable(s) -> body }
+*/
+
+fun main() {
+    var sum: (Int, Int) -> Int = { a: Int, b: Int -> a + b }
+    println(sum(10,5))
+
+    var sum2 = {a: Int, b: Int -> println(a + b) }
+    sum2(10, 5)
+
+    var sum3: (Double, Int) -> Double = {a, b -> a + b}
+    println(sum3(10.5, 5))
+
+    var str: (String) -> Unit = {it -> println("$it !!")}
+    str("String")
+
+    var str2: (String) -> String = {it -> "$it !!"}
+    println(str2("String"))
+
+}
