74ac5694d7a3780de7ca1185d1e673d6b8379477
sungyong-lim <dlatjddyd524@gmail.com>
2022-07-10 16:44:30 +0900

    SafeCastOperator Update

--- Kotlin/src/main/kotlin/more/SafeCastOperator.kt
+++ Kotlin/src/main/kotlin/more/SafeCastOperator.kt
@@ -1,5 +1,21 @@
 package more
 
+/** 불안전 캐스트 연산자(Unsafe Cast Operator)와 안전한 캐스트 연산자(Safe Cast Operator) **/
+
+/*
+    * 불안전 캐스트 연산자
+        - 어떤 때는 변수를 캐스트하지 못하고 예외 처리가 됨
+        - 특정 타입의 것을 다른 타입으로 캐스트 하는 것
+        - "as" 키워드 사용
+        - "infix" 연산자 "as"에 의해 실행됨
+
+    * 안전한 캐스트 연산자
+        - "as?" 키워드 사용
+        - 한 타입으로 안전하게 캐스트하도록 도와줌
+        - 캐스팅할 수 없을 시 "ClassCastException" 처리 대신에 null 값을 부여함
+
+*/
+
 fun main() {
     /** Unsafe Cast Operator Test **/
 
