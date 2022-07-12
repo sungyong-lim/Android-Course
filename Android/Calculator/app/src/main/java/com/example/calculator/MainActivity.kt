3b9cfff5566b5401d5c5abd8259007cf0566cb57
sungyong-lim <dlatjddyd524@gmail.com>
2022-07-12 17:53:16 +0900

    Calculator Button Update

--- Android/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Android/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSeven.setOnClickListener { toastFunc(btnSeven) }
+        btnEight.setOnClickListener { toastFunc(btnEight) }
+        btnNine.setOnClickListener { toastFunc(btnNine) }
+        btnDiv.setOnClickListener { toastFunc(btnDiv) }
+
+
+
+    }
+
+
+    private fun toastFunc(v: View) {
+        when(v.id) {
+            v.id ->  Toast.makeText(this,"selected ", Toast.LENGTH_SHORT).show()
+        }
     }
 }
