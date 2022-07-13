5d5199d7651776dbcc6a4632ad3cfd1c5c4f87d2
sungyong-lim <dlatjddyd524@gmail.com>
2022-07-13 16:46:24 +0900

    Question UI Design Update

--- Android/FlagQuizApp/app/src/main/java/com/example/flagquizapp/QuestionsActivity.kt
+++ Android/FlagQuizApp/app/src/main/java/com/example/flagquizapp/QuestionsActivity.kt
@@ -3,9 +3,11 @@ package com.example.flagquizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.android.synthetic.main.activity_questions.*
+import android.widget.ImageView
+import android.widget.TextView
 
 class QuestionsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questions)
