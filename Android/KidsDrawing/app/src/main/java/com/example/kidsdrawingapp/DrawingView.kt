48bc8a0c52340f97519662a526d338efe77c28cd
sungyong-lim <dlatjddyd524@gmail.com>
2022-07-18 18:35:17 +0900

    Create SizeChanged, Draw Func

--- Android/KidsDrawing/app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
+++ Android/KidsDrawing/app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -31,6 +31,20 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs){
         mBrushSize = 20.toFloat() // 브러쉬 사이즈 설정
     }
 
+    // DrawingView 가 View 를 상속받기 때문에 하위 요소(onSizeChanged)를 재정의 가능 
+    // 뷰의 크기가 변경되었을 때 레이아웃 안에 설정됨
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888) // config = Configuration 즉 배열은 원하는 걸로 사용 가능
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    // 뷰가 자체를 그리는 데 사용할 수 있음
+    // 뭔가를 적거나 그릴 캔버스를 포함
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+//        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f)
+    }
 
     // CustomPath 클래스를 DrawingView 내부에서만 사용 => internal
     // 변수를 가져오거나 내보낼 수 있음 => inner
